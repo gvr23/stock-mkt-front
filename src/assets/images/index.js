ab8dba354ae761ffe26b1c84ba6a2a175d3cbf22
Giovani Rubini <giovanirubinimaccagno@gmail.com>
2019-10-20 21:16:13 -0500

    manage admin

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,2 +1,5 @@
 export const updown = require('./up-and-down-arrows.png');
 export const pricepig = require('./piggy-bank.png');
+export const play = require('./play.png');
+export const pause = require('./pause.png');
+export const stop = require('./stop.png');
